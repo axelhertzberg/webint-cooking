9bba95373f411ff1da0758a8c247888d1f9732ee
axelhertzberg <axelher@student.chalmers.se>
2023-01-22 12:14:24 +0100

    Initial setup commit: * Fix all imports and install

--- cooking-gurus/src/App.js
+++ cooking-gurus/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import RestaurantsPage from "./pages/RestaurantsPage";
+import { Container } from "react-bootstrap";
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                                //icons
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container >
+      <RestaurantsPage />
+    </Container>
   );
 }
 
