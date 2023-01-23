3fe167917f17ce02cb1284123c8c8ed0a7e5163c
axelhertzberg <axelher@student.chalmers.se>
2023-01-23 15:43:36 +0100

    Fix in home button

--- cooking-gurus/src/components/HomeButton.js
+++ cooking-gurus/src/components/HomeButton.js
@@ -2,12 +2,14 @@ import React from 'react'
 import {Button} from "primereact/button"
 
 export default function HomeButton() {
+
+
     return (
         <div style={{marginBottom: "20px"}}>
             <Button 
                 className='home-button' 
                 label="Home" icon="pi pi-home" 
-                onClick={() => window.location.href = '/webint-cooking'}/>
+                onClick={() => window.location.href = '/webint-cooking/index.html'}/>
         </div>
     )
 }
