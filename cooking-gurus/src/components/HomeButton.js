e9a9dc428ec9b629a00c0b82bb7cd13a2f2f2249
axelhertzberg <axelher@student.chalmers.se>
2023-01-26 21:54:55 +0100

    fix home button

--- cooking-gurus/src/components/HomeButton.js
+++ cooking-gurus/src/components/HomeButton.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import {Button} from "primereact/button"
+import {useNavigate} from "react-router-dom";
 
 export default function HomeButton() {
 
+    const navigate = useNavigate()
 
     return (
         <div style={{marginBottom: "20px"}}>
             <Button 
                 className='home-button' 
                 label="Home" icon="pi pi-home" 
-                onClick={() => window.location.href = '/webint-cooking/index.html'}/>
+                onClick={() => navigate("/")}/>
         </div>
     )
 }
